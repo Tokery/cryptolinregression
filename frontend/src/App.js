9aeb1551938092b758ea7345e0d7870fc2914ee9
kevincott97@gmail.com <kevincott97@gmail.com>
2018-05-24 21:44:03 -0400

    Added dygraph to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import Dygraph from 'dygraphs';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    const g = new Dygraph('graph',
+    `Date,A,B
+    2016/01/01,10,20
+    2016/07/01,20,10
+    2016/12/31,40,30
+    `, {
+      fillGraph: true
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +25,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id="graph"></div>
       </div>
     );
   }
