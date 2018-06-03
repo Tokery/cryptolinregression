49fc7c1617f519608f687c020ae84c6bf8987ae0
kevincott97@gmail.com <kevincott97@gmail.com>
2018-06-03 13:38:25 -0400

    Data is now sourced from a csv in the backend and set via websockets to the frontend

--- backend/api/channels.py
+++ backend/api/channels.py
@@ -1,9 +1,19 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
+import time
+import csv
+import os
 
 class DataConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
+        with open('./FB.csv', newline='') as csvfile:
+            print ("Got here")
+            stock_reader = csv.reader(csvfile)
+            # Skip header row
+            next(stock_reader)
+            self.stock_data = list(stock_reader)
+
 
     def disconnect(self, close_code):
         pass
@@ -12,7 +22,17 @@ class DataConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
-
+        # data = 0
+        # while data < 10:
+        #     data += 1
+        #     self.send(text_data=json.dumps({
+        #         'x': self.stock_data[data][0],
+        #         'y': self.stock_data[data][5],
+        #     }))
+        #     time.sleep(1)
+        for time_value in self.stock_data:
+            self.send(text_data=json.dumps({
+                'x': time_value[0],
+                'y': time_value[5],
+            }))
+            time.sleep(1)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.data = [];
-    const t = new Date();
-    for (var i = 10; i >= 0; i--) {
-      var x = new Date(t.getTime() - i * 1000);
-      this.data.push([x, Math.random()]);
-    }
+    // So how to populate this to begin with???
+    this.data = [[new Date("2017-01-03"), 116.860001]];
 
     // The following should be moved to Redux
     this.state = {
@@ -23,28 +19,27 @@ class App extends Component {
   componentDidMount(){
     const g = new Dygraph('graph',
     this.data, {
-      fillGraph: true,
+      fillGraph: false,
+      labels: ["Time", "Price"]
     });
-    const parent = this;
-    // It sucks that these things aren't objects, and we need to store state in window.
-    window.intervalId = setInterval(function() {
-      var x = new Date();  // current time
-      var y = Math.random();
-      parent.data.push([x, y]);
-      g.updateOptions( { 'file': parent.data } );
-    }, 1000);
+    console.log('Mounted');
     
     const host = "ws://localhost:8000/";
     this.socket = new WebSocket(host);
 
     this.socket.onopen = () => {
       console.log('Socket opened!!');
-      this.socket.send(JSON.stringify({ message: 'Hello' }));
+      this.socket.send(JSON.stringify({ message: 'Ready for data' }));
     }
     
     this.socket.onmessage = (message) => {
+      message = JSON.parse(message.data);
       console.log('Received ', message);
       this.setState({ wsMessage: message });
+
+      const { x, y } = message; 
+      this.data.push([new Date(x), y]);
+      g.updateOptions({ 'file': this.data });
     }
 
   }
