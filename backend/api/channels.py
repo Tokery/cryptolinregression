147b6682e2298df0f56291d7d446598fe90d07e1
kevincott97@gmail.com <kevincott97@gmail.com>
2018-06-11 22:29:09 -0400

    Added 95% prediction interval. App now does buy and sell simulation!!

--- backend/api/channels.py
+++ backend/api/channels.py
@@ -3,17 +3,44 @@ import json
 import time
 import csv
 import os
+import math
+
+import numpy as np
+from sklearn import linear_model
+from scipy import stats
+from scipy.stats import t
+from functools import reduce
 
 class DataConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    def _getFileData(self):
         with open('./FB.csv', newline='') as csvfile:
-            print ("Got here")
             stock_reader = csv.reader(csvfile)
             # Skip header row
             next(stock_reader)
-            self.stock_data = list(stock_reader)
+            day = 0
+            for row in stock_reader:
+                day += 1
+                self.dates.append(day)
+                self.realDays.append(row[0])
+                self.prices.append(float(row[5]))
 
+    def connect(self):
+        self.accept()
+        self.dates = []
+        self.prices = []
+        self.realDays = []
+        self._getFileData()
+        
+        # self.linear_mod = linear_model.LinearRegression() #defining the linear regression
+        # dates = np.reshape(self.dates, (len(self.dates), 1)) # convert to nx1 matrix (n rows, 1 column)
+        # prices = np.reshape(self.prices, (len(self.prices), 1)) 
+        #self.linear_mod.fit(dates, prices) # fit the data points to the model (Learn from the model)
+        self.x_bar = sum(self.dates) / len(self.dates)
+        self.s_xx = reduce((lambda x, y: x + (y - self.x_bar)**2), self.dates)
+        self.slope, self.intercept, _, _, std_err = stats.linregress(self.dates, self.prices)
+        self.s_e = std_err * math.sqrt(self.s_xx)
+        
+        
 
     def disconnect(self, close_code):
         pass
@@ -21,18 +48,13 @@ class DataConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
-        # data = 0
-        # while data < 10:
-        #     data += 1
-        #     self.send(text_data=json.dumps({
-        #         'x': self.stock_data[data][0],
-        #         'y': self.stock_data[data][5],
-        #     }))
-        #     time.sleep(1)
-        for time_value in self.stock_data:
+        
+        for index, price in enumerate(self.prices):
             self.send(text_data=json.dumps({
-                'x': time_value[0],
-                'y': time_value[5],
+                'x': self.realDays[index],
+                'y': price,
+                # 'prediction': self.linear_mod.predict(index + 1)[0][0]
+                'prediction': self.slope * index + self.intercept,
+                'error': self.s_e * math.sqrt(1 + (1 / len(self.dates)) + ((index - self.x_bar)**2)/self.s_xx) * t.ppf(0.025, len(self.dates)- 2)
             }))
-            time.sleep(1)
+            time.sleep(0.25)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,21 +9,42 @@ class App extends Component {
     super(props);
 
     // So how to populate this to begin with???
-    this.data = [[new Date("2017-01-03"), 116.860001]];
+    this.data = [[new Date("2017-01-03"), 116.860001, 116.860001]];
 
     // The following should be moved to Redux
     this.state = {
       wsMessage: '',
+      bought: [],
+      sold: [],
+      profit: 0,
     }
   }
   componentDidMount(){
-    const g = new Dygraph('graph',
+    this.drawInitialGraph();
+    this.handleWebsocket();
+  }
+
+  drawInitialGraph() {
+    this.g = new Dygraph('graph',
     this.data, {
       fillGraph: false,
-      labels: ["Time", "Price"]
+      labels: ["Time", "Price", "Prediction"],
+      underlayCallback: function(canvas, area, g) {
+        console.log('Called me');
+        var bottom_left = g.toDomCoords(new Date("2017-01-04"), -20);
+        var top_right = g.toDomCoords(new Date("2017-01-05"), +20);
+        console.log(bottom_left, area);
+
+        var left = bottom_left[0];
+        var right = top_right[0];
+
+        canvas.fillStyle = "rgba(255, 255, 102, 1.0)";
+        canvas.fillRect(left, area.y, right - left, area.h);
+      }
     });
-    console.log('Mounted');
-    
+  }
+
+  handleWebsocket() {
     const host = "ws://localhost:8000/";
     this.socket = new WebSocket(host);
 
@@ -37,12 +58,30 @@ class App extends Component {
       console.log('Received ', message);
       this.setState({ wsMessage: message });
 
-      const { x, y } = message; 
-      this.data.push([new Date(x), y]);
-      g.updateOptions({ 'file': this.data });
+      const { x, y, prediction, error } = message; 
+      if (y - prediction > Math.abs(error)) {
+        const sold = this.state.sold;
+        const profit = this.state.profit + y;
+        sold.push(y);
+        this.setState({
+          sold,
+          profit,
+        });
+      }
+      else if (prediction - y > Math.abs(error)) {
+        const bought = this.state.bought;
+        const profit = this.state.profit - y;
+        bought.push(y);
+        this.setState({
+          bought,
+          profit,
+        });
+      }
+      this.data.push([new Date(x), y, prediction]);
+      this.g.updateOptions({ 'file': this.data });
     }
-
   }
+
   render() {
     return (
       <div className="App">
@@ -51,10 +90,25 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Last price: {this.state.wsMessage.y}
         </p>
         <div style={{ position: 'relative', width: 'inherit', height: '500px' }}>
           <div id="graph" style={{ position: 'absolute', bottom: '10px', right: '10px', left: '10px', top: '10px'}}></div>
+        </div>
+        <button onClick={this.buy}>Buy</button>
+        <button onClick={this.sell}>Sell</button>
+        <div>
+          Profit: {this.state.profit}
+        </div>
+        <div>
+          { this.state.bought.map(value => {
+            return <div>Bought: {value}</div>
+          })}
+        </div>
+        <div>
+          { this.state.sold.map(value => {
+            return <div>Sold: {value}</div>
+          })}
         </div>
       </div>
     );
